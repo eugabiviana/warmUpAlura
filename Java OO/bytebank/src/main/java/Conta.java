23ff17df17a948c43c9a84f2842f9d6ce4a16e7b
eugabiviana <gabi.viana88@gmail.com>
2023-04-03 15:49:57 -0300

    Java OO - criando metodo deposita

--- Java OO/bytebank/src/main/java/Conta.java
+++ Java OO/bytebank/src/main/java/Conta.java
@@ -4,4 +4,13 @@ public class Conta{
     int agencia;
     int numero;
     String titular;
+
+    //Criando um método
+    void deposita(double valor){
+        this.saldo = this.saldo + valor;
+    }
 }
+
+//As cores de saldo e valor diferem, porque saldo é um ATRIBUTO da classe Conta e valor é uma VARIÁVEL temporária, ou seja,
+// fechou o } ela "morre";
+//this se refere ao ATRIBUTO SALDO da Classe! Por isso usamos no método, para deixar isso claro!
