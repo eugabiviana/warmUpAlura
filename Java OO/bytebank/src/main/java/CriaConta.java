cc6216fbfd4fdab4386312c00f8ceda79adfd91a
eugabiviana <gabi.viana88@gmail.com>
2023-04-03 12:56:46 -0300

    Java OO - entendendo a independecia dos valores em contas diferentes

--- Java OO/bytebank/src/main/java/CriaConta.java
+++ Java OO/bytebank/src/main/java/CriaConta.java
@@ -1,8 +1,19 @@
 public class CriaConta {
     public static void main(String[] args) {
-        //instanciação/ inicialização através do new
+        //instancialização/ inicialização através do new
         Conta primeiraConta = new Conta();
         primeiraConta.saldo = 200;
         System.out.println(primeiraConta.saldo);
+
+        primeiraConta.saldo += 100;
+        System.out.println(primeiraConta.saldo);
+
+        Conta segundaConta = new Conta();
+        segundaConta.saldo = 50;
+
+        System.out.println("Saldo da primeira conta: " + primeiraConta.saldo);
+        System.out.println("Saldo da segunra conta: " + segundaConta.saldo);
+
+
     }
 }
