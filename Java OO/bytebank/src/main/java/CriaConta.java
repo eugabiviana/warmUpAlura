582d9d15a4e57a11a7ab2a6a9eddbefec9c13bf6
eugabiviana <gabi.viana88@gmail.com>
2023-04-03 12:46:10 -0300

    Java OO - criando uma conta e entendendo os primeiros conceitos bÃasicos de instancia e inicializacao de objeto

---
+++ Java OO/bytebank/src/main/java/CriaConta.java
@@ -0,0 +1,8 @@
+public class CriaConta {
+    public static void main(String[] args) {
+        //instanciação/ inicialização através do new
+        Conta primeiraConta = new Conta();
+        primeiraConta.saldo = 200;
+        System.out.println(primeiraConta.saldo);
+    }
+}
